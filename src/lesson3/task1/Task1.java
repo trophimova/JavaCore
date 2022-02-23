7b5ec9d7929684b9007e4cf936f4e3eb3e70143e
Anya <48622247+trophimova@users.noreply.github.com>
2022-02-23 13:50:56 +0300

    lesson2

---
+++ src/lesson3/task1/Task1.java
@@ -0,0 +1,28 @@
+package lesson3.task1;
+
+import java.util.Arrays;
+
+public class Task1 {
+    static  String[] arrayString = {"1", "2", "3", "4"};
+    static Integer[] arrayInteger = {1, 2, 3};
+    static Double[] arrayDouble = {1d, 2d, 3d, 4d};
+
+    public static void main(String[] args) {
+        SwapArrayClass<Object> swapArrayClass = new SwapArrayClass<>();
+        try {
+            System.out.println(Arrays.toString(swapArrayClass.swapElements(arrayString, 0, 3)));
+        } catch (ArrayIndexOutOfBoundsException exception) {
+            exception.printStackTrace();
+        }
+        try {
+            System.out.println(Arrays.toString(swapArrayClass.swapElements(arrayInteger, 0, 2)));
+        } catch (ArrayIndexOutOfBoundsException exception) {
+            exception.printStackTrace();
+        }
+        try {
+            System.out.println(Arrays.toString(swapArrayClass.swapElements(arrayDouble, 0, 3)));
+        } catch (ArrayIndexOutOfBoundsException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
